8f04c8329f7c2c419c04f347bca230d0a1dc681c
Nostrada <prikulmax2003@gmail.com>
2023-12-26 17:55:17 +0700

    without axios

---
+++ front/src/page/Home.tsx
@@ -0,0 +1,35 @@
+import { AboutCompany, Advantages, Contact, MainScreen, Partners, Services } from "../components";
+import { Footer, Nav } from "../section";
+
+const Home = () => {
+    return (
+        <div>
+            <section>
+                <Nav />
+            </section>
+            <section>
+                <MainScreen />
+            </section>
+            <section  className="pt-16 bg-bg-main">
+                <Advantages />
+            </section>
+            <section  className="pt-16 bg-main-dark">
+                <Services />
+            </section>
+            <section  className=" bg-bg-main">
+                <AboutCompany />
+            </section>
+            <section  className="pt-16 bg-main-dark">
+                <Partners />
+            </section>
+            <section  className=" bg-bg-main">
+                <Contact />
+            </section>
+            <section  className=" pt-16 bg-[#414141]">
+                <Footer />
+            </section>
+        </div>
+    )
+}
+
+export default Home
